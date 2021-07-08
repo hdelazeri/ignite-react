4753f88e742c108e1fa41ebe44949797097cd80f
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-08 20:29:43 -0300

    Styling of the repository list
    Adding styling to the repository list

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,13 +1,7 @@
 import { RepositoryList } from './components/RepositoryList'
-import { Counter } from './components/Counter'
 
 import './styles/global.scss'
 
 export function App() {
-  return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
-  )
+  return <RepositoryList />
 }
