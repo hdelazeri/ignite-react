7998c38d983822bd395b7ff38f27e4a0aa625b91
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-15 18:32:34 -0300

    Adding transaction type buttons
    Creating and styling the transaction type buttons

--- 02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -31,7 +33,21 @@ export function NewTransactionModal({
         <h2>Cadastrar Transação</h2>
 
         <input placeholder="Título" />
+
         <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
--- 02-dtmoney/src/components/NewTransactionModal/styles.ts
+++ 02-dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,3 +1,4 @@
+import { darken } from "polished";
 import styled from "styled-components";
 
 export const Container = styled.form`
@@ -44,6 +45,43 @@ export const Container = styled.form`
 
     &:hover {
       filter: brightness(0.9);
+    }
+  }
+`
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+
+  button {
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+    
+    background: transparent;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: 0.2s border-color;
+
+    &:hover {
+      border-color: ${darken(0.1, '#d7d7d7')}
+    }
+
+    img {
+      height: 20px;
+      width: 20px;
+    }
+
+    span {
+      display: inline-block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text-title);
     }
   }
 `
