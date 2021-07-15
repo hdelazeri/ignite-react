6ea10da8ddafd57d9116f0dd4ef14e32fd275eae
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-15 20:42:27 -0300

    Create a Transactions hook
    Creating a transactions hook to encapsulate the transactions

--- 02-dtmoney/src/hooks/useTransactions.tsx
+++ 02-dtmoney/src/hooks/useTransactions.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { api } from "../services/api";
 
 type Transaction = {
@@ -21,7 +27,7 @@ type TransactionsContextData = {
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 };
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -50,4 +56,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
