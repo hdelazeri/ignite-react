7d73dea57cc3d906c19b2e490a0f871ca66ef2a7
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-21 17:17:17 -0300

    Listing all the posts available

--- 03-ignews/src/pages/posts/index.tsx
+++ 03-ignews/src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import { getPrismicClient } from "../../services/prismic";
 
 import styles from "../../styles/posts.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+type PostsProps = {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -15,38 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              voluptates atque in sed velit ipsam hic. Officia dolorum aut vitae
-              esse atque accusantium minima? Incidunt error dolores ab aperiam
-              numquam!
-            </p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              voluptates atque in sed velit ipsam hic. Officia dolorum aut vitae
-              esse atque accusantium minima? Incidunt error dolores ab aperiam
-              numquam!
-            </p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              voluptates atque in sed velit ipsam hic. Officia dolorum aut vitae
-              esse atque accusantium minima? Incidunt error dolores ab aperiam
-              numquam!
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p> {post.excerpt} </p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -61,9 +48,25 @@ export const getStaticProps: GetStaticProps = async () => {
     { fetch: ["post.title", "post.content"], pageSize: 100 }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: { posts },
   };
 };
