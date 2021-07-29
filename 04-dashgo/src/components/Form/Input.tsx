62d2eed5cc0b9be1c13fee1a82f65ca7ed385d38
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-29 14:02:03 -0300

    Moving the input to a separate component

---
+++ 04-dashgo/src/components/Form/Input.tsx
@@ -0,0 +1,30 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+
+type InputProps = ChakraInputProps & {
+  name: string;
+  label?: string;
+};
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        id={name}
+        name={name}
+        focusBorderColor="pink.500"
+        bg="gray.900"
+        variant="filled"
+        size="lg"
+        _hover={{ bg: "gray.900" }}
+        {...rest}
+      />
+    </FormControl>
+  );
+}
