2e2f1b3d76d96f642f50810f997c177fcc7a90f1
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-30 20:22:53 -0300

    Making the dashboard responsive

--- 04-dashgo/src/pages/dashboard.tsx
+++ 04-dashgo/src/pages/dashboard.tsx
@@ -52,7 +52,7 @@ export default function Dashboard() {
           minChildWidth="320px"
           alignSelf="flex-start"
         >
-          <Box p="8" bg="gray.800" rounded="8" pb="4">
+          <Box p={["6", "8"]} bg="gray.800" rounded="8" pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
@@ -60,7 +60,7 @@ export default function Dashboard() {
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
-          <Box p="8" bg="gray.800" rounded="8" pb="4">
+          <Box p={["6", "8"]} bg="gray.800" rounded="8" pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
