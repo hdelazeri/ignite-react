b3e5046ebf0b8f4ae3331a884bc282f12cae5ae3
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-30 19:54:22 -0300

    Making the header responsive

--- 04-dashgo/src/components/Header/Profile.tsx
+++ 04-dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+type ProfileProps = {
+  showProfileData?: boolean;
+};
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Henrique Delazeri</Text>
-        <Text color="gray.300" fontSize="small">
-          contato@hdelazeri.dev
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Henrique Delazeri</Text>
+          <Text color="gray.300" fontSize="small">
+            contato@hdelazeri.dev
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
