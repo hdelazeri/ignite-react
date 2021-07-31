b40127c3a661d41c0ad9381f3933e07470544285
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-31 20:45:56 -0300

    Adding the navigation paths

--- 04-dashgo/src/components/Sidebar/NavLink.tsx
+++ 04-dashgo/src/components/Sidebar/NavLink.tsx
@@ -1,19 +1,22 @@
-import { Icon, Link, LinkProps, Text } from "@chakra-ui/react";
-import { ElementType } from "react";
+import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
 import { IconType } from "react-icons/lib";
+import Link from "next/link";
 
 type NavLinkProps = {
   icon: IconType;
   children: string;
+  href: string;
 } & LinkProps;
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">
-        {children}
-      </Text>
+    <Link href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
   );
 }
