23306cc5f669d2f98870abe990ed413db395f1b0
Henrique Delazeri <personal@hdelazeri.dev>
2021-08-01 14:59:57 -0300

    Validating the data of the sign in form
    Understanding how to validate the data of the sign in form using the yup validator

--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import { Input } from "../components/Form/Input";
 
 type SignInFormData = {
@@ -7,8 +10,17 @@ type SignInFormData = {
   password: string;
 };
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória"),
+});
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const { errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -32,6 +44,7 @@ export default function SignIn() {
             name="email"
             label="E-mail"
             type="email"
+            error={errors.email}
             {...register("email")}
           />
 
@@ -39,6 +52,7 @@ export default function SignIn() {
             name="password"
             label="Senha"
             type="password"
+            error={errors.password}
             {...register("password")}
           />
         </Stack>
