a75c8c8298404849671db9ad463784869cbe3a86
Henrique Delazeri <personal@hdelazeri.dev>
2021-08-01 14:42:06 -0300

    Adding basic functionality to the sign in form
    Understanding the basics of the react hook form library and adding it to the sign in form

--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,7 +1,20 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,13 +25,31 @@ export default function SignIn() {
         p="8"
         rounded="8"
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Senha" type="password" />
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+            {...register("email")}
+          />
+
+          <Input
+            name="password"
+            label="Senha"
+            type="password"
+            {...register("password")}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
