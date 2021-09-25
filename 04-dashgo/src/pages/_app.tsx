65f5ad2d6a7c4310d9d63e5cc8ece92971b4edd4
Henrique Delazeri <personal@hdelazeri.dev>
2021-09-25 15:20:54 -0300

    Displaying user data loaded from the API

--- 04-dashgo/src/pages/_app.tsx
+++ 04-dashgo/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import { ChakraProvider } from "@chakra-ui/react";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 
@@ -22,6 +24,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -28,7 +28,22 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
     
-    return data
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    return users
+  }, {
+    staleTime: 1000 * 5
   })
 
   const isWideVersion = useBreakpointValue({
@@ -86,56 +101,26 @@ export default function UserList() {
                 </Thead>
 
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
 
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Henrique Delazeri</Text>
-                        <Text fontSize="small" color="gray.300">
-                          contato@hdelazeri.dev
-                        </Text>
-                      </Box>
-                    </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="small" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
 
-                    {isWideVersion && <Td>04 de abril de 2021</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Henrique Delazeri</Text>
-                        <Text fontSize="small" color="gray.300">
-                          contato@hdelazeri.dev
-                        </Text>
-                      </Box>
-                    </Td>
-
-                    {isWideVersion && <Td>04 de abril de 2021</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Henrique Delazeri</Text>
-                        <Text fontSize="small" color="gray.300">
-                          contato@hdelazeri.dev
-                        </Text>
-                      </Box>
-                    </Td>
-
-                    {isWideVersion && <Td>04 de abril de 2021</Td>}
-                  </Tr>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    )
+                  })}
                 </Tbody>
               </Table>
 
