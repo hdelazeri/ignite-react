297ec3f47f4160a1325906897ed88c7fbbff1bf4
Henrique Delazeri <personal@hdelazeri.dev>
2021-09-25 15:10:14 -0300

    Setting up react-query

--- 04-dashgo/src/pages/_app.tsx
+++ 04-dashgo/src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ChakraProvider } from "@chakra-ui/react";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 
 import { theme } from "../styles/theme";
@@ -10,13 +12,17 @@ if (process.env.NODE_ENV === 'development') {
   makeServer()
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import {
   Box,
   Button,
@@ -6,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -16,22 +16,25 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
+import { useQuery } from 'react-query'
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+    
+    return data
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
 
   return (
     <Box>
@@ -59,74 +62,86 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
 
-                <Th>Usuário</Th>
+                    <Th>Usuário</Th>
 
-                {isWideVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Henrique Delazeri</Text>
-                    <Text fontSize="small" color="gray.300">
-                      contato@hdelazeri.dev
-                    </Text>
-                  </Box>
-                </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Henrique Delazeri</Text>
+                        <Text fontSize="small" color="gray.300">
+                          contato@hdelazeri.dev
+                        </Text>
+                      </Box>
+                    </Td>
 
-                {isWideVersion && <Td>04 de abril de 2021</Td>}
-              </Tr>
+                    {isWideVersion && <Td>04 de abril de 2021</Td>}
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Henrique Delazeri</Text>
-                    <Text fontSize="small" color="gray.300">
-                      contato@hdelazeri.dev
-                    </Text>
-                  </Box>
-                </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Henrique Delazeri</Text>
+                        <Text fontSize="small" color="gray.300">
+                          contato@hdelazeri.dev
+                        </Text>
+                      </Box>
+                    </Td>
 
-                {isWideVersion && <Td>04 de abril de 2021</Td>}
-              </Tr>
+                    {isWideVersion && <Td>04 de abril de 2021</Td>}
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Henrique Delazeri</Text>
-                    <Text fontSize="small" color="gray.300">
-                      contato@hdelazeri.dev
-                    </Text>
-                  </Box>
-                </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Henrique Delazeri</Text>
+                        <Text fontSize="small" color="gray.300">
+                          contato@hdelazeri.dev
+                        </Text>
+                      </Box>
+                    </Td>
 
-                {isWideVersion && <Td>04 de abril de 2021</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
+                    {isWideVersion && <Td>04 de abril de 2021</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
